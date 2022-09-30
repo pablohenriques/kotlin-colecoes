cf21d6d7c5bb78cb9f6f6b5e85c1eeaada1aea46
Pablo Henrique <ph.soft.servicos@gmail.com>
2022-09-29 21:21:10 -0300

    Indices e arrays

--- src/main/kotlin/Salarios.kt
+++ src/main/kotlin/Salarios.kt
@@ -1,13 +1,10 @@
 fun main() {
     val salarios: DoubleArray = doubleArrayOf(1500.50, 2300.0, 5000.0, 8000.0, 10000.0)
-
     val aumento = 1.1
 
-    var indice = 0
-    for(salario in salarios) {
-        salarios[indice] = salario * aumento
-        indice++
+
+    for(indice in salarios.indices) {
+        salarios[indice] = salarios[indice] * aumento
     }
     println(salarios.contentToString())
-
 }
